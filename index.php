c19a416b0a19b4c33e7c7ec8d0b0dc0f7fb622d9
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-18 13:43:30 +0200

    add test push

--- index.php
+++ index.php
@@ -49,10 +49,31 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
 </head>
 
 <body>
-
+    <div class="container mt-2">
+        <h1 class="text-white text-center">Hotel</h1>
+        <div class="separator"></div>
+        <ul>
+            <?php foreach ($hotels as $hotel) : ?>
+                <li class="text-white">
+                    <p><? echo $hotel['name']  ?></p>
+                    <p><? echo $hotel['description']  ?></p>
+                    <?php if ($hotel['parking'] === true) : ?>
+                        <p>yes</p>
+                    <?php else : ?>
+                        <p>no</p>
+                    <?php endif ?>
+                    <p><? echo $hotel['vote']  ?></p>
+                    <p><? echo $hotel['distance_to_center']  ?></p>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
 </body>
 
 </html>
